54eb284343b8063b8412aa522fa2bdf05f5cb2cb
Hyung Tak Jin <njir@naver.com>
2016-04-13 10:31:01 +0000

    feat: Add concept.js

---
+++ concept.js
@@ -0,0 +1,18 @@
+var read_line = require('read-line')
+var fs = require('fs')
+
+var input = fs.createReadStream(__dirname + '/log.txt')
+input.pipe(read_line)
+read_line.on('readable', function() {
+    var line
+    while (line = read_line.read()) {
+        var arr = line.toString().split(" ")
+        var strJson = {
+            time: arr[0],
+            type: arr[1],
+            event_id: arr[2]
+                // ... add
+        }
+        console.log(strJson)
+    }
+})
