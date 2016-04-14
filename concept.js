279b4bdcf5eaec4afd000fda3d041b5a8f930346
Tak <njir@users.noreply.github.com>
2016-04-14 09:55:51 +0900

    chore: Add object & delete space

--- concept.js
+++ concept.js
@@ -1,17 +1,17 @@
 var read_line = require('read-line')
 var fs = require('fs')
 
-var input = fs.createReadStream(__dirname + '/log.txt')
+var input = fs.createReadStream(__dirname + '/logfile.txt')
 input.pipe(read_line)
 read_line.on('readable', function() {
     var line
     while (line = read_line.read()) {
         var arr = line.toString().split(" ")
         var strJson = {
-            time: arr[0],
-            type: arr[1],
-            event_id: arr[2]
-            // ... add
+            time: arr[0].replace(/\s/g, ''),
+            type: arr[1].replace(/\s/g, ''),
+            event_name: arr[2].replace(/\s/g, ''),
+            event_description: arr[3].replace(/\s/g, '')
         }
         console.log(strJson)
         // strJson to Graph using kibana
