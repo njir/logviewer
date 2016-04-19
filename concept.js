129e72649c55ff37129dbc347418d07cccfb57c8
Tak <njir@users.noreply.github.com>
2016-04-19 17:02:12 +0900

    chore: Add source code field

--- concept.js
+++ concept.js
@@ -6,12 +6,13 @@ input.pipe(read_line)
 read_line.on('readable', function() {
     var line
     while (line = read_line.read()) {
-        var arr = line.toString().split(" ")
+        var arr = line.toString().split(";")
         var strJson = {
             time: arr[0].replace(/\s/g, ''),
             type: arr[1].replace(/\s/g, ''),
             event_name: arr[2].replace(/\s/g, ''),
-            event_description: arr[3].replace(/\s/g, '')
+            event_description: arr[3].replace(/\s/g, ''),
+            code: arr[6].replace(/\s/g, '')
         }
         console.log(strJson)
         // strJson to Graph using kibana
