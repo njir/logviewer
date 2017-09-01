90e1a2d8967429cdd37bc924a81310f231b69626
Tak <njir@users.noreply.github.com>
2017-09-01 16:20:29 +0900

    chore: Modify cout code for speed
    just '\n' is faster than 'endl' because 'endl' includes 'flush'
    I did not know until now...

--- CpuUsage/main.cpp
+++ CpuUsage/main.cpp
@@ -1,5 +1,6 @@
 #include <Pdh.h>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <stdlib.h>
 #include "CpuUsage.h"
@@ -9,13 +10,14 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	if (!checkValidArugment(argc, argv)) {
-		cout << "Usage: CpuUsage.exe [output] [measure time]\n" << endl;
-		cout << "Example: ParsingXML.exe c15.csv 60\n" << endl;
+		cout << "Usage: CpuUsage.exe [output] [measure time]\n" << '\n';
+		cout << "Example: ParsingXML.exe c15.csv 60\n" << '\n';
+		cin.get();
 		exit(1);
 	}
 
 	// start
-	cout << "Setup Cpu Query... Please wait for seconds\n" << endl;
+	cout << "Setup Cpu Query... Please wait for seconds\n" << '\n';
 
 	CpuUsage *myCpu = new CpuUsage;
 	ofstream outputFile(argv[1], ios::trunc);
@@ -24,13 +26,13 @@ int main(int argc, char* argv[])
 	double echoLoaderUsage = 0;
 	double average = 0;
 
-	cout << "Write CPU Usage to " << argv[1] << endl;
+	cout << "Write CPU Usage to " << argv[1] << '\n';
 	outputFile << "Time,Total Cpu\n";
 	
 	for (int i = 0; i < atoi(argv[2]); i++) {
 		totalUsage = myCpu->getTotalUsage();
 		
-		cout << "Cpu Usage: " << totalUsage << "%" << endl;
+		cout << std::internal << std::setw(3) << i << ". Cpu Usage: " << fixed << setprecision(4) << totalUsage << "%" << '\n';
 		outputFile << i << "," << totalUsage << "\n";
 
 		if(i != 0) // drop the first data
@@ -40,7 +42,7 @@ int main(int argc, char* argv[])
 
 	// calculate average
 	average = (average / (atoi(argv[2]) - 1));
-	cout << "Average: " << average << "%" << endl;
+	cout << "Average: " << average << "%" << '\n';
 	outputFile << "Average" << "," << average << "\n";
 
 	delete myCpu;
